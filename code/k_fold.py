2edb5debe9a92fa1ee7b9332ef1205caf0d0e555
gigium <gigim1995@gmail.com>
2020-01-04 17:04:54 +0100

    add leave one out method

--- code/k_fold.py
+++ code/k_fold.py
@@ -1,14 +1,36 @@
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import LeaveOneOut
 import pandas as pd
 import os
 import sys
+from feature_selection import read_data
 
 
 
-def read_data(file_name):
-	df = pd.read_csv(file_name, sep="\t", index_col=0)
-	return df
+def leaveOneOut(df):
+	# target and data selection
+	y=df.iloc[:,-1]
+	X=df.iloc[:,:-1]
+	y=y.to_numpy()
+
+	loo = LeaveOneOut()
+	loo.get_n_splits(X,y)
+
+	
+	split={
+	"train":[],
+	"test":[]
+	}
+	
+	for train_index, test_index in loo.split(X, y):
+		split["train"].append(train_index)
+		split["test"].append(test_index)
+	
+	print("leave one out ... created ", len(split["train"]))
+
+	return split
+
 
 
 def stratifiedKfold(df, k=5):
@@ -29,11 +51,13 @@ def stratifiedKfold(df, k=5):
 		split["train"].append(train_index)
 		split["test"].append(test_index)
 	
+	print("stratified k fold ... k = ", k)
+
 	return split
 
 
 
-def  createFolder(df,split_dict,k,path="./kFold"):
+def  createFolder(df, split_dict, path="./kFold"):
 	access_rights = 0o777
 
 	try:
@@ -54,13 +78,17 @@ def  createFolder(df,split_dict,k,path="./kFold"):
 
 def main():
 	data = read_data(sys.argv[1])
-	k=int(sys.argv[2])
-	split=stratifiedKfold(data,k)
-	print(split)
-	createFolder(data,split,k)
+	if sys.argv[2] == "stratified":
+		k=int(sys.argv[3])
+		split=stratifiedKfold(data,k)
+		createFolder(data, split)
+
+	elif sys.argv[2] == "loo":
+		split=leaveOneOut(data)
+		createFolder(data, split, path="./loo")
 
 
 
-# python stratified_k_fold.py .\data\new_data.txt 3
+# python k_fold.py .\data\new_data.txt [stratified | loo] {k}
 if __name__== "__main__":
   main()
