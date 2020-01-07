48adb5801f3a09388923ab7daa0b10c4c79c5c12
federico-code <federicodelaurentiis7@gmail.com>
2020-01-07 19:40:10 +0100

    adding experiment 3_

--- code/main.py
+++ code/main.py
@@ -1,12 +1,12 @@
 import os, sys
 from feature_selection import read_data
-from experiments import runExperiment, experiment3_0_1
+from experiments import runExperiment, experiment3_2
 import mlflow
 
 
 def main():
 	DIR = sys.argv[1]
-	runExperiment(DIR, experiment3_0_1, [10,100,1000,10000,20000])
+	runExperiment(DIR, experiment3_2, [0.1, 0.3, 0.5, 0.7, 0.8, 0.9, 0.95, 0.99])
 
 
 # python main.py .\kFold
